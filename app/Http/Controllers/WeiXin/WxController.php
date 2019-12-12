e0f38815152e7a48cae8a882c4ffd9ec266f098f
chelize <chelize@gmail.com>
2019-12-12 02:26:10 +0000

    weixin

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -4,12 +4,11 @@ namespace App\Http\Controllers\WeiXin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Foundation\Bootstrap\HandleExceptions;
 class WxController extends Controller
 {
     public function wechat()
     {
-        dd(111);
         $token = '2259b56f5898cd6192c50d338723d9e4';       //开发提前设置好的 token
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
