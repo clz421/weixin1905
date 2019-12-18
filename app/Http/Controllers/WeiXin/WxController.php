6a415cc186c6af7a83f5354a220e8e781f54b3d5
chelize <chelize@gmail.com>
2019-12-18 06:08:36 +0000

    wx

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -9,22 +9,22 @@ class WxController extends Controller
 {
     public function wechat()
     {
-        // $token = '2259b56f5898cd6192c50d338723d9e4';       //开发提前设置好的 token
-        // $signature = $_GET["signature"];
-        // $timestamp = $_GET["timestamp"];
-        // $nonce = $_GET["nonce"];
-        // $echostr = $_GET["echostr"];
+        $token = '2259b56f5898cd6192c50d338723d9e4';       //开发提前设置好的 token
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $echostr = $_GET["echostr"];
 
-        // $tmpArr = array($token, $timestamp, $nonce);
-        // sort($tmpArr, SORT_STRING);
-        // $tmpStr = implode( $tmpArr );
-        // $tmpStr = sha1( $tmpStr );
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
 
-
-        // if( $tmpStr == $signature ){        //验证通过
-        //     echo $echostr;
-        // }else{
-        //     die("not ok");
-        // }
+        if( $tmpStr == $signature ){        //验证通过
+            echo $echostr;
+        }else{
+            die("not ok");
+        }
     }
+
 }
